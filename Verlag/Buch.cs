2ca0de058c76c12e89e38b5a7d4fb83fbc2268ba
Ugur Günay <ugurg3@gso.schule.koeln>
2023-01-16 09:45:18 +0100

    update code

---
+++ Verlag/Buch.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Verlag
+{
+    internal class Buch
+    {
+        private string autor;
+        private string titel;
+        private int auflage;
+
+        public Buch (string autor, string titel)
+        {
+            this.autor = autor;
+            this.titel = titel;
+            this.auflage = 1;
+        }
+
+        public Buch(string autor, string titel, int auflage):this(autor,titel)
+        {
+           
+            this.auflage = auflage;
+        }
+
+        
+    }
+}
